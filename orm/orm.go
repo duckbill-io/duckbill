1c4f1d276eca38bae16b3e601c7338a4e81fce01
mindz <lianjie1949@gmail.com>
2019-05-24 19:04:33 +0800

    cler

--- orm/orm.go
+++ orm/orm.go
@@ -17,52 +17,57 @@ func Scan(v interface{}) error {
 
 // scan　实际查找对应数据，并更新实例的接口
 func scan(v reflect.Value) error {
-	switch v.Kind() {
-	case reflect.Ptr:
-		ve := v.Elem()
-		if v.IsNil() {
-			return fmt.Errorf("cann't scan nil Ptr")
-		}
-		if ve.Kind() == reflect.Struct {
-			return unmarshal(v)
-		}
-		if ve.Kind() == reflect.Slice || ve.Kind() == reflect.Array {
-			prepare := func(v reflect.Value) (names []string, err error) {
-				_filepath := "_data/metas"
-				f, err := os.Open(_filepath)
-				if err != nil {
-					return
-				}
-				list, err := f.Readdir(-1)
-				f.Close()
-				if err != nil {
-					return
-				}
-				names = make([]string, 0, len(list))
-				for i := range list {
-					name := strings.Split(list[i].Name(), ".")[0]
-					names = append(names, name)
-				}
+	if v.Kind() != reflect.Ptr {
+		return fmt.Errorf("must be Ptr!")
+	}
+
+	if v.IsNil() {
+		return fmt.Errorf("cann't scan nil Ptr")
+	}
+
+	ve := v.Elem()
+	switch ve.Kind() {
+	case reflect.Struct:
+		return unmarshal(v)
+	case reflect.Slice, reflect.Array:
+		prepare := func(v reflect.Value) (names []string, err error) {
+			_filepath := "_data/metas"
+			f, err := os.Open(_filepath)
+			if err != nil {
 				return
 			}
-			if ve.Len() < 1 {
-				return nil
-			}
-			names, err := prepare(ve.Index(0))
+			list, err := f.Readdir(-1)
+			f.Close()
 			if err != nil {
-				return err
+				return
 			}
-			t := ve.Index(0).Elem().Type()
-			for i := 0; i < len(names); i++ {
-				item := reflect.New(t)
-				item.Elem().FieldByName("Name").SetString(names[i])
-				scan(item)
-				ve.Set(reflect.Append(ve, item))
+			names = make([]string, 0, len(list))
+			for i := range list {
+				name := strings.Split(list[i].Name(), ".")[0]
+				names = append(names, name)
 			}
+			return
+		}
+
+		if ve.Len() < 1 {
+			return nil // TODO 这里需要考虑能不能传个长度为0的slice
+		}
+
+		names, err := prepare(ve.Index(0))
+		if err != nil {
+			return err
+		}
+
+		for t, i := ve.Index(0).Elem().Type(), 0; i < len(names); i++ {
+			item := reflect.New(t)
+			item.Elem().FieldByName("Name").SetString(names[i])
+			scan(item)
+			ve.Set(reflect.Append(ve, item))
 		}
 	default:
-		return fmt.Errorf("must be a Ptr")
+		return fmt.Errorf("cann't scan type: %s", ve.Type())
 	}
+
 	return nil
 }
 
