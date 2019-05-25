2b88203294c1e6f518f923bb3c53eb6c2b82f456
mindz <lianjie1949@gmail.com>
2019-05-25 17:29:26 +0800

    draft: 完成标签功能的控制器草稿

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -31,3 +31,24 @@ func Posts(w io.Writer) {
 		log.Fatal(err)
 	}
 }
+
+// 显示制定名称的标签
+func ShowTag(w io.Writer, name string) {
+	tag, err := models.FindTag(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	showTemplate := "views/tags/show.html"
+	render.Execute(w, showTemplate, tag)
+}
+
+// 罗列所有的标签
+func Tags(w io.Writer) {
+	tags, err := models.FindAllTags()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	indexTemplate := "views/tags/index.html"
+	render.Execute(w, indexTemplate, tags)
+}
