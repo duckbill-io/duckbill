8765f55f6afeae7ca5266848e9a2a5e7c4a3e29f
mindz <lianjie1949@gmail.com>
2019-06-01 22:45:53 +0800

    feat: 记录请求时记录处理耗时

--- routes/middleware.go
+++ routes/middleware.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -13,7 +14,9 @@ type middleware func(httprouter.Handle) httprouter.Handle
 // logReq 中间件用于记录访问请求
 func logReq(h httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		log.Printf("%s %q", r.Method, r.URL.Path)
+		timeStart := time.Now()
 		h(w, r, ps)
+		timeElapsed := time.Since(timeStart)
+		log.Printf("%s %q %s", r.Method, r.URL.Path, timeElapsed)
 	}
 }
